d2a408547fac57c91544afc9b23778dd3f2b7886
gk44739 <gk44739@ubt-uni.net>
2021-05-21 20:55:32 +0200

    Routes Init

---
+++ tms-web-front/src/Routes/Routes.js
@@ -0,0 +1,10 @@
+import React, { lazy, Suspense } from 'react'
+import {Switch, Route } from 'react-router-dom';
+
+const routes = [];
+
+const Routes = (props) =>{
+    
+}
+
+export default Routes;
