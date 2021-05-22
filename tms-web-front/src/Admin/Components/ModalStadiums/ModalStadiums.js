05637bece873fe3288eeb17e18d6ccdd98743c57
lirijon <lirijon98gmail.com>
2021-05-22 19:22:28 +0200

    styling the modals

--- tms-web-front/src/Admin/Components/ModalStadiums/ModalStadiums.js
+++ tms-web-front/src/Admin/Components/ModalStadiums/ModalStadiums.js
@@ -1,40 +1,42 @@
-import './ModalStadiums.css';
+import './ModalStadiums.css'
 
-const ModalStadiums = () =>{
-
+const ModalStadiums = (props) =>{
     return(
-    <div className="modal" tabindex="-1">
-        <div className="modal-dialog">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Modal title</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+       <div className={`modalCustom ${props.openModal ? "active" : ""}`}>
+           <div className="modalContent">
+                <div className="modalRemove" onClick={()=>{props.closeModalHeandler()}}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="329pt"
+                        height="329pt"
+                        viewBox="0 0 329.269 329"
+                        >
+                        <path d="M194.8 164.77L323.013 36.555c8.343-8.34 8.343-21.825 0-30.164-8.34-8.34-21.825-8.34-30.164 0L164.633 134.605 36.422 6.391c-8.344-8.34-21.824-8.34-30.164 0-8.344 8.34-8.344 21.824 0 30.164l128.21 128.215L6.259 292.984c-8.344 8.34-8.344 21.825 0 30.164a21.266 21.266 0 0015.082 6.25c5.46 0 10.922-2.09 15.082-6.25l128.21-128.214 128.216 128.214a21.273 21.273 0 0015.082 6.25c5.46 0 10.922-2.09 15.082-6.25 8.343-8.34 8.343-21.824 0-30.164zm0 0"></path>
+                    </svg>
                 </div>
-                <div className="modal-body">
-                <form>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
+                <div className="modalBodyCustom">
+                    <form style={{width: "300px"}}>
+                        <div className="mb-3">
+                            <label className="form-label">Name</label>
+                            <input type="text" name="name" className="form-control" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Capacity</label>
+                            <input type="number" className="form-control" name="capacity" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Image</label>
+                            <input type="file" className="form-control" name="image" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Rank</label>
+                            <input type="number" className="form-control" name="rank" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Save</button>
+                    </form>
                 </div>
             </div>
-        </div>
-    </div>
+       </div>
     );
 }
 
--- tms-web-front/src/Admin/Components/ModalTeams/ModalTeams.js
+++ tms-web-front/src/Admin/Components/ModalTeams/ModalTeams.js
@@ -1,40 +1,66 @@
 import './ModalTeams.css'
 
-const ModalTeams = () =>{
-
+const ModalTeams = (props) =>{
     return(
-    <div className="modal" tabindex="-1">
-        <div className="modal-dialog">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Modal title</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+       <div className={`modalCustom ${props.openModal ? "active" : ""}`}>
+           <div className="modalContent">
+                <div className="modalRemove" onClick={()=>{props.closeModalHeandler()}}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="329pt"
+                        height="329pt"
+                        viewBox="0 0 329.269 329"
+                        >
+                        <path d="M194.8 164.77L323.013 36.555c8.343-8.34 8.343-21.825 0-30.164-8.34-8.34-21.825-8.34-30.164 0L164.633 134.605 36.422 6.391c-8.344-8.34-21.824-8.34-30.164 0-8.344 8.34-8.344 21.824 0 30.164l128.21 128.215L6.259 292.984c-8.344 8.34-8.344 21.825 0 30.164a21.266 21.266 0 0015.082 6.25c5.46 0 10.922-2.09 15.082-6.25l128.21-128.214 128.216 128.214a21.273 21.273 0 0015.082 6.25c5.46 0 10.922-2.09 15.082-6.25 8.343-8.34 8.343-21.824 0-30.164zm0 0"></path>
+                    </svg>
                 </div>
-                <div className="modal-body">
-                <form>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
+                <div className="modalBodyCustom">
+                    <form style={{width: "300px"}}>
+                        <div className="mb-3">
+                            <label className="form-label">Name</label>
+                            <input type="text" name="name" className="form-control" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">City</label>
+                            <input type="text" className="form-control" name="city" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Logo</label>
+                            <input type="number" className="form-control" name="logo" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Founded Year</label>
+                            <input type="number" className="form-control" name="foundedYear" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Manager</label>
+                            <input type="text" className="form-control" name="manager" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Trophies</label>
+                            <input type="number" className="form-control" name="trophies" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Owner</label>
+                            <input type="text" className="form-control" name="owner" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Budget</label>
+                            <input type="number" className="form-control" name="budget" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">League</label>
+                            <input type="text" className="form-control" name="league" />
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Stadium</label>
+                            <input type="text" className="form-control" name="stadium" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Save</button>
+                    </form>
                 </div>
             </div>
-        </div>
-    </div>
+       </div>
     );
 }
 
