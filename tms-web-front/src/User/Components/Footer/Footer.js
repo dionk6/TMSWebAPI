71510845f1c881100479b3c33778cf68b8efadaf
lirijon <lirijon98gmail.com>
2021-05-27 21:45:05 +0200

    creating header and footer

--- tms-web-front/src/User/Components/Footer/Footer.js
+++ tms-web-front/src/User/Components/Footer/Footer.js
@@ -1,9 +1,26 @@
+import './Footer.css';
 
 const Footer = (props) =>{
 
     return(
         <footer className="footer">
-            <h1>Footer</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="footerLogo col-md-4">
+                        <h1>Footer</h1>
+                    </div>
+                    <div className="footerLinks col-md-4">
+                        <ul>    
+                            <l1>L</l1>
+                            <l1>A</l1>
+                            <l1>L</l1>
+                        </ul>
+                    </div>
+                    <div className=" col-md-4">
+
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 
--- tms-web-front/src/User/Components/Header/Header.js
+++ tms-web-front/src/User/Components/Header/Header.js
@@ -1,12 +1,35 @@
+import './Header.css';
 
 const Header = (props) =>{
 
     return(
         <header className="header">
-            <h1>Header</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="logoHeader col-md-4">
+                        <img src="" />
+                    </div>
+                    <nav className="headersLinks col-md-8">
+                        <ul>
+                            <li className="navItem">
+                                <a>Home</a>
+                            </li>
+                            <li className="navItem">
+                                <a>Leagues</a>
+                            </li>
+                            <li className="navItem">
+                                <a>About</a>
+                            </li>
+                            <li className="navItem">
+                                <a>Contact</a>
+                            </li>    
+                        </ul>    
+                    </nav>
+                </div>
+            </div>
         </header>
     );
 
 }
 
-export default Header
+export default Header;
