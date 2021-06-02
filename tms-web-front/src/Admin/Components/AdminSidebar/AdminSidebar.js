04b6df3d89de509fabc9ea562c311c2ce71062bb
gk44739 <gk44739@ubt-uni.net>
2021-06-02 21:11:56 +0200

    Admin side bar notifications

--- tms-web-front/src/Admin/Components/AdminSidebar/AdminSidebar.js
+++ tms-web-front/src/Admin/Components/AdminSidebar/AdminSidebar.js
@@ -20,7 +20,7 @@ const AdminSidebar = (props) =>{
                 </div>
                 <nav>
                     <ul>
-                        <li><NavLink exact to="/Admin">Leauges</NavLink></li>
+                        <li><NavLink exact to="/Admin">Leagues</NavLink></li>
                         <li><NavLink to="/Admin/Players">Players</NavLink></li>
                         <li><NavLink to="/Admin/Stadiums">Stadiums</NavLink></li>
                         <li><NavLink to="/Admin/Teams">Teams</NavLink></li>
