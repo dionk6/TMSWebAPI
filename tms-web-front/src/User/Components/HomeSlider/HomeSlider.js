97017b2ebd692b431e8c9ca06384a995afce80a0
gk44739 <gk44739@ubt-uni.net>
2021-06-03 22:41:22 +0200

    Layout changes

--- tms-web-front/src/User/Components/HomeSlider/HomeSlider.js
+++ tms-web-front/src/User/Components/HomeSlider/HomeSlider.js
@@ -1,49 +1,52 @@
-import './HomeSlider.css';
-import {useState , useEffect} from 'react';
+import "./HomeSlider.css";
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
 import "swiper/components/scrollbar/scrollbar.scss";
-import HomeSlide from '../../Components/HomeSlide/HomeSlide';
-import {TeamsTable} from '../../../http/http-requests';
+import HomeSlide from "../../Components/HomeSlide/HomeSlide";
+import { TeamsTable } from "../../../http/http-requests";
 
-const HomeSlider = (porps) =>{
-    const [teams , setTeams] = useState([]);
+const HomeSlider = (porps) => {
+  const [teams, setTeams] = useState([]);
 
-    const allTeams = async () =>{
-        const allTeams = await TeamsTable()
-        setTeams(allTeams.data);
-    }
+  const allTeams = async () => {
+    const allTeams = await TeamsTable();
+    setTeams(allTeams.data);
+  };
 
-    useEffect(()=>{
-       allTeams();
-    },[]) 
+  useEffect(() => {
+    allTeams();
+  }, []);
 
-    return(
-        <Swiper
-            slidesPerView={1}
-            effect="fade"
-            loop={true}
-            autoplay={{
-                delay: 4000,
-                disableOnInteraction: false,
-            }}
-        >
-            {
-                teams.map((team,i)=>{
-                    return(
-                    <SwiperSlide key={i}>
-                        <HomeSlide teamImage={team.logo} teamName={team.name} city={team.city} manager={team.manager} trophies={team.trophies}/>
-                    </SwiperSlide>
-                    )
-                })
-                
-            }
-        </Swiper>
-    );
-    
-
-}
+  return (
+    <Swiper
+      slidesPerView={1}
+      effect="fade"
+      loop={true}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+    >
+      {teams.map((team, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <HomeSlide
+              teamImage={team.logo}
+              teamName={team.name}
+              city={team.city}
+              manager={team.manager}
+              trophies={team.trophies}
+              league={team.league}
+              stadium={team.stadium}
+            />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
 
 export default HomeSlider;
