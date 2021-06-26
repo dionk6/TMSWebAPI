961a2d20aa8005fc8a417748ec1e145a3faa7758
Alban salihu <albi.imi@live.com>
2021-06-26 17:18:46 +0200

    Player Card End Point Read

--- tms-web-front/src/User/Components/PlayerCard/PlayerCard.js
+++ tms-web-front/src/User/Components/PlayerCard/PlayerCard.js
@@ -1,41 +1,39 @@
 import "./PlayerCard.css";
-import Ibra from "../../Images/ibra.png"
 
-const PlayerCard = () => {
+const PlayerCard = (props) => {
+    console.log(props.player)
     return (
         <div className="PlayerCardPage ">
             <div className="container">
                 <div className="row mb-5">
                     <div className="col-lg-4 PlayerImg">
-                        <img src={Ibra}/>
+                        <img src={props.player.photo}/>
                     </div>
                     <div className="col-lg-8 PlayerInfo p-4">
                         <div className="row">
                             <div className="col-lg-12">
                                 <div className="row">
                                     <div className="col-lg-10 mt-5 text-secondary">
-                                        <h6>Name <span className="text-white FontSize"> ZLATAN IBRAHIMOVIC</span></h6>
-                                        <h6>Postion <span className="text-white FontSize"> ATTACKER</span></h6>
-                                        <h6>Age <span className="text-white FontSize"> 39</span></h6>
+                                        <h6>Name <span className="text-white FontSize"> {props.player.firstName +" "+ props.player.lastName}</span></h6>
+                                        <h6>Postion <span className="text-white FontSize"> {props.player.position}</span></h6>
+                                        <h6>Age <span className="text-white FontSize"> {props.player.age}</span></h6>
                                     </div>
                                     <div className="col-lg-2 mt-5">
-                                        <h3 className="Kit">10</h3>
+                                        <h3 className="Kit">{props.player.playerNo}</h3>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-12">
                             <h3 className="Red">INFORMATION</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo dicta iure atque dolorem voluptate quasi maxime, repellat aliquid eos. Beatae molestias itaque aspernatur quaerat culpa laudantium ducimus est eveniet. Repudiandae!lorem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo dicta iure atque dolorem voluptate quasi maxime, repellat aliquid eos. Beatae molestias itaque aspernatur quaerat culpa laudantium ducimus est eveniet. Repudiandae!lorem.<br/>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo dicta iure atque dolorem voluptate quasi maxime, repellat aliquid eos. He made his debut for France in October 2011 and featured in four of Les Bleus' five games at the 2014 World Cup.
+                            <p>
+                                {props.player.bio}
                             </p>
                         </div>
                         <div className="col-lg-12">
                             <h3 className="Red">TEAM</h3>
                             <p>
-                            Lorem ipsum dolor sit amet,<br/>
-                            Lorem ipsum dolor sit amet,<br/>
-                            Lorem ipsum dolor sit amet,<br/>
+                                {props.player.team}
                             </p>
                         </div>
                         <div className="BuyNowWrapper">
--- tms-web-front/src/User/Pages/Players/Players.js
+++ tms-web-front/src/User/Pages/Players/Players.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import "./Players.css";
 import PlayerCard from "../../Components/PlayerCard/PlayerCard";
-import PlayerBanner from "../../Components/PlayerBanner/PlayerBanner"
+import PlayerBanner from "../../Components/PlayerBanner/PlayerBanner";
+import {playersHttpRequestTable} from "../../../http/http-requests";
 
 const Players = () => {
+    const [Players,setPlayers] = useState([]);
+    const getPlayers = async () => {
+        const allPlayers = await playersHttpRequestTable();
+        setPlayers(allPlayers.data);
+    }
+    useEffect(() => {
+        getPlayers();
+    },[])
     return (
         <div className="PlayersPage">
             <div className="mb-5">
                 <PlayerBanner/>
             </div>
-                <PlayerCard/>
+                {
+                    Players.map((player, key)=> { 
+                        return <PlayerCard player = {player} key = {key}/>
+                    })
+                }
         </div>
     );
 }
